9d18fd68254ff68625a79fe154b2436833308910
Jonathan L <jlemanski1@gmail.com>
2018-04-20 20:57:16 -0600

    Added Paddles & Player movement
    -Added Paddles w/ collision
    -Added basic player movement

---
+++ Pong/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+    [SerializeField]
+    private int speed;     // Paddle's move speed
+
+    private void Awake() {
+        speed = 30;     // Set speed
+    }
+
+    private void FixedUpdate () {
+        // Vertical key press
+        float vertInput = Input.GetAxisRaw("Vertical");
+
+        //Set Paddle's velocity to the vertical input
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, vertInput) * speed;
+	}
+}
