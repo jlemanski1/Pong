49c1445e20c110f57a2a408a7c4fb63b0942a7e8
Jonathan L <jlemanski1@gmail.com>
2018-04-20 22:40:08 -0600

    SplashScreen, BGMusic, Menu/Countdown
    - Added Splashscreen/ Menu countdown Scene
    - Added Soft BG Music
    - TODO: 
        - Update the colours of the walls & paddles to match spashscreen
        - Resize the Paddles to be a bit thinner so they aren't  as chunky
        - Resize the Score UI and added ex: (Score: {})

---
+++ Pong/Assets/Scripts/MusicPersistance.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class MusicPersistance : MonoBehaviour {
+	
+    //  Preserve gameObject through Scenes
+	private void Update () {
+        DontDestroyOnLoad(gameObject);
+	}
+}
---
+++ Pong/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour {
+
+    public string sceneToLoad;      // Name of next scene
+
+    public int sceneLoadDelay;      // In seconds
+
+    public Text countdownText;
+    private float countdown;
+
+    // Call OpenNextScene() after the set delay
+	private void Awake () {
+        Invoke("OpenNextScene", sceneLoadDelay);
+        countdown = sceneLoadDelay;
+        countdownText = GameObject.Find("Countdown").GetComponent<Text>();      // Get Countdown text
+	}
+	
+	// Transitions to the next scene
+	private void OpenNextScene () {
+        SceneManager.LoadScene(sceneToLoad);
+	}
+
+    // Update menu UI with load countdown
+    private void Update() {
+        // While Scene is still delaying load
+        for (int i = 0; i < sceneLoadDelay; i++) {
+            countdown -= Time.deltaTime / 10;
+            countdownText.text = Mathf.RoundToInt(countdown).ToString();
+            
+        }
+    }
+}
